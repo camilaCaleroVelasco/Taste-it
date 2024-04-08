4c88aa98f636fc17396b31a2d19197e5d83ed319
batulhc <Batul.chitalwala@uga.edu>
2024-04-08 15:19:50 -0400

    Create page.js

---
+++ app/Profile/page.js
@@ -0,0 +1,57 @@
+'use client'
+import React, {useState} from 'react'
+import {useRouter} from 'next/navigation'
+import Link from 'next/link'
+import Hdr from '../components/Hdr'
+import './page.css'
+
+const ProfilePage = () => {
+
+
+
+    const USER_RECIPES = [
+      {
+        id: '1',
+        img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRI--fVLjutQhCS9ioPEY9jdcweIw6K-bnBii3JyDJ6lg&s',
+        name: 'Omelet',
+        text: 'Eggs fried in olive oil, folded around delicious fillings.'
+        
+      },
+      {
+        id: '2',
+        img: 'https://i.imgur.com/67Lp5fh.jpg',
+        name: 'Pho',
+        text: 'A popular Vietnamese soup dish with rice noodles.'
+        
+      },
+      {
+        id: '3',
+        img: 'https://tastesbetterfromscratch.com/wp-content/uploads/2023/02/Banana-Bread-6.jpg',
+        name: 'Banana Bread',
+        text: 'Sweet bread made from mashed bananas, sometimes with nuts added. '
+        
+      },
+      {
+        id: '4',
+        img: 'https://www.onceuponachef.com/images/2024/02/chipotle-chicken-quesadillas.jpg',
+        name: 'Quesadillas',
+        text: 'A Mexican dish of tortillas stuffed with fillings then cooked on a griddle or stove.'
+        
+      },
+    ];
+  
+    const[foods, setFoods] = useState(USER_RECIPES);
+  
+    return (
+      <div className="profile"> 
+           <Hdr />
+           <h1 className="yourTitle">Your Recipes</h1>
+           
+        
+        <FoodList items={foods}/>
+      </div>
+    );
+  }
+  
+  
+  export default ProfilePage;
