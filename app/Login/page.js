e17308f9de3d85755bec33cf66171926e59aff6e
samtwo468 <43520741+samtwo468@users.noreply.github.com>
2024-04-08 13:51:18 -0400

    added login form

--- app/Login/page.js
+++ app/Login/page.js
@@ -1,11 +1,32 @@
+
+'use client'
+import React, {useState} from 'react'
+import {useRouter} from 'next/navigation'
 import Link from 'next/link'
 import Hdr from '../components/Hdr'
+import './page.css'
 
 const LoginPage = () => {
+
+    const router = useRouter()
+
+    const submitHandler = (event) => {
+        event.preventDefault()
+        router.push('/') //change once we make the 'logged in user screen'
+    }
+
     return (
         <div>
             <Hdr />
-            <Link href="/">Login Placeholder</Link>
+            <form onSubmit={submitHandler}>
+                <label>Username: </label>
+                <input type="text"/>
+                <br></br>
+                <label>Password: </label>
+                <input type="text"/>
+                <br></br>
+                <button type="submit">Log In</button>
+            </form>
         </div>
     )
 }
